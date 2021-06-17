2ea23500e632a243e15635ad2d543956874fd5fb
hbelleus <33425237+hbelleus@users.noreply.github.com>
2021-06-17 05:38:08 +0200

    Test if operation is saved when withdrawal: KO

--- src/test/java/com/sfeir/kata/bank/domain/ClientOperationWithdrawalTest.java
+++ src/test/java/com/sfeir/kata/bank/domain/ClientOperationWithdrawalTest.java
@@ -3,12 +3,15 @@ package com.sfeir.kata.bank.domain;
 import java.math.BigDecimal;
 
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
+import org.assertj.core.data.Index;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
 import com.sfeir.kata.bank.domain.client.ClientOperation;
+import com.sfeir.kata.bank.domain.operation.Operation;
 import com.sfeir.kata.bank.domain.operation.money.Money;
 import com.sfeir.kata.bank.utils.BankClientFactory;
 
@@ -38,5 +41,26 @@ class ClientOperationWithdrawalTest {
 
 		// THEN
 		Assertions.assertThat(result).isInstanceOf(Boolean.class);
+	}
+
+	@Test
+	void givenPositiveAmount_WhenWithdrawal_thenOperationIsSaved() {
+
+		// GIVEN
+		var amount = Money.of(BigDecimal.valueOf(100));
+
+		// WHEN
+		var result = client.withdrawal(amount);
+
+		// THEN
+
+		Condition<Operation> savedOperation = new Condition<>(
+				(operation) -> operation.getAmount().equals(amount.toNegative()),
+				"checking if saved operation has the correct amount", amount);
+
+		org.junit.jupiter.api.Assertions.assertAll(() -> Assertions.assertThat(result).isTrue(),
+				() -> Assertions.assertThat(client.getAccount().getHistory().getOperations()).isNotEmpty()
+						.has(savedOperation, Index.atIndex(0)));
+
 	}
 }
