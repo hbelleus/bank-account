8b60e1e36d36ec179c790274391a1fb09c50df5d
hbelleus <33425237+hbelleus@users.noreply.github.com>
2021-06-18 02:45:07 +0200

    adding cucumber test for printing functionality

---
+++ src/test/java/com/sfeir/kata/bank/behaviour/printing/ClientPrintingOperationStepDefinition.java
@@ -0,0 +1,64 @@
+package com.sfeir.kata.bank.behaviour.printing;
+
+import java.io.PrintStream;
+import java.math.BigDecimal;
+
+import org.mockito.Mockito;
+
+import com.sfeir.kata.bank.domain.client.ClientOperation;
+import com.sfeir.kata.bank.domain.money.Money;
+import com.sfeir.kata.bank.domain.printer.ConsolePrinter;
+import com.sfeir.kata.bank.domain.printer.StatementPrinter;
+import com.sfeir.kata.bank.utils.BankClientMockFactory;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ClientPrintingOperationStepDefinition {
+
+	private PrintStream printStream;
+	private StatementPrinter printer;
+	private ClientOperation client;
+
+	@Before
+	public void setUp() {
+		printStream = Mockito.mock(PrintStream.class);
+		printer = new ConsolePrinter(printStream);
+		client = BankClientMockFactory.create(printer);
+	}
+
+	@Given("^I deposit (\\d+) euros$")
+	public void deposit(BigDecimal amount) {
+
+		client.deposit(Money.of(amount));
+
+	}
+
+	@Given("^I withdraw (\\d+) euros$")
+	public void withdraw(BigDecimal amount) {
+
+		client.withdrawal(Money.of(amount));
+
+	}
+
+	@When("^I print the operations$")
+	public void i_print_the_operations() {
+
+		client.printOperationHistory();
+
+	}
+
+	@Then("it should print {string}")
+	public void it_should_print(String message) {
+		org.junit.jupiter.api.Assertions.assertAll(() -> Mockito.verify(printStream).println(message),
+				() -> Mockito.verifyNoMoreInteractions(printStream));
+	}
+
+	@Then("^the printer should be called (\\d+) times$")
+	public void printer_is_called_3_times(int value) {
+		Mockito.verify(printStream, Mockito.times(value)).println(Mockito.anyString());
+		;
+	}
+}
